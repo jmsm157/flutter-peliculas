b2229042a241ef34de4f118a5947b699e91d95a4
Johan Sánchez <jmsm157@gmail.com>
2022-05-08 20:12:34 -0500

    ep 125 al 128

---
+++ lib/models/credits_response.dart
@@ -0,0 +1,30 @@
+// To parse this JSON data, do
+//
+//     final creditsResponse = creditsResponseFromJson(jsonString);
+
+// ignore_for_file: prefer_if_null_operators
+
+import 'dart:convert';
+
+import 'models.dart';
+
+class CreditsResponse {
+  CreditsResponse({
+    required this.id,
+    required this.cast,
+    required this.crew,
+  });
+
+  int id;
+  List<Cast> cast;
+  List<Cast> crew;
+
+  factory CreditsResponse.fromJson(String str) =>
+      CreditsResponse.fromMap(json.decode(str));
+
+  factory CreditsResponse.fromMap(Map<String, dynamic> json) => CreditsResponse(
+        id: json["id"],
+        cast: List<Cast>.from(json["cast"].map((x) => Cast.fromMap(x))),
+        crew: List<Cast>.from(json["crew"].map((x) => Cast.fromMap(x))),
+      );
+}
